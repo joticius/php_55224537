383e7e7bdd11250937cda7f3df225ac8dfa5fdcc
joticius <jjgarciaf@uniboyaca.edu.co>
2026-05-06 16:06:19 -0500

    Correcciones II

--- Vista/index_calc.php
+++
@@ -1,8 +0,0 @@
-<?php
-require_once __DIR__ . '/Controlador/CalculatorController.php';
-
-$controller = new CalculatorController();
-$data = $controller->handleRequest();
-
-include __DIR__ . '/Vista/calculator.php';
-?>
